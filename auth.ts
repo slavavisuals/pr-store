940300617d640807349bc16c360f41459e2f7d09
slavavisuals <slavavisuals@gmail.com>
2025-07-22 21:09:28 -0600

    adding validations

--- auth.ts
+++ auth.ts
@@ -53,6 +53,8 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       // Set the user ID from the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
       // if there is an update, set the user name
       if (trigger) {
@@ -60,6 +62,30 @@ export const config = {
       }
 
       return session;
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role;
+
+        // If user has no name, use email as their default name
+        if (user.name === 'NO_NAME') {
+          token.name = user.email!.split('@')[0];
+
+          // Update the user in the database with the new name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+
+      // Handle session updates (e.g., name change)
+      if (session?.user.name && trigger === 'update') {
+        token.name = session.user.name;
+      }
+
+      return token;
     },
   },
 } satisfies NextAuthConfig;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,11 @@
 'use server';
 
-import { signInFormSchema } from '../validators';
+import { signInFormSchema, signUpFormSchema } from '../validators';
 import { signIn, signOut } from '@/auth';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
+import { hashSync } from 'bcrypt-ts';
+import { prisma } from '@/db/prisma';
+import { formatError } from '../utils';
 
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
   try {
@@ -27,4 +30,43 @@ export async function signInWithCredentials(prevState: unknown, formData: FormDa
 export async function signOutUser() {
   // get current users cart and delete it so it does not persist to next user
   await signOut({ redirectTo: '/' });
+}
+
+// Sign up user
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      confirmPassword: formData.get('confirmPassword'),
+      password: formData.get('password'),
+    });
+
+    const plainPassword = user.password;
+    user.password = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: 'User created successfully' };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
 }
